df9a094a3c8ee4968510b4c67e9c9dcdc3d0140b
hvdave <hdave@stumail.jccc.edu>
2013-05-20 22:59:55 -0500

    changed server portx1

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
 var http = require("http");
 
-http.createServer(function(request, response) {
+function onRequest(request, response) {
+  console.log("Request received.");
   response.writeHead(200, {"Content-Type": "text/plain"});
-  response.write("Hello World\n");
+  response.write("Hello World");
   response.end();
-}).listen(8080);
+}
+
+http.createServer(onRequest).listen(8080);
+
+console.log("Server has started.");
