0fafd5a95d4c7ca753324170e04541cc2b47be95
hvdave <hdave@stumail.jccc.edu>
2013-05-20 22:11:47 -0500

    changed server host1

--- server.js
+++ server.js
@@ -2,6 +2,6 @@ var http = require('http');
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
   res.end('Hello World\n');
-}).listen(80, 'ec2-23-22-219-226.compute-1.amazonaws.com');
+}).listen(80);
 console.log('Server running at http://ec2-23-22-219-226.compute-1.amazonaws.com/');
 
