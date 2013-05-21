adae28955488a01ba3b4a8c54b1ee157862c6a82
hvdave <hdave@stumail.jccc.edu>
2013-05-20 22:30:20 -0500

    changed server port

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
-var http = require('http');
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World\n');
-}).listen(80);
+var http = require("http");
 
+http.createServer(function(request, response) {
+  response.writeHead(200, {"Content-Type": "text/plain"});
+  response.write("Hello World\n");
+  response.end();
+}).listen(8888);
