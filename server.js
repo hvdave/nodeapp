dc8ed2a46e8185a5e9f835010c0a3a49450330bf
hvdave <hdave@stumail.jccc.edu>
2013-05-20 23:19:23 -0500

    added url path

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 var http = require("http");
+var url = require("url");
 
 function start(){
 	function onRequest(request, response) {
- 	 console.log("Request received.");
-  	response.writeHead(200, {"Content-Type": "text/plain"});
-  	response.write("Hello World");
-  	response.end();
+ 		console.log("Request received.");
+		var pathname = url.parse(request.url).pathname;
+		console.log("Request for : "+ pathname + " received.");
+  		response.writeHead(200, {"Content-Type": "text/plain"});
+  		response.write("Hello World");
+  		response.end();
 	}
 
 	http.createServer(onRequest).listen(8080);
